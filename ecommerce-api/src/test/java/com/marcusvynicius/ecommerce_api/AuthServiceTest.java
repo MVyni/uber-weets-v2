41dc16eba4e9ff94ae5bb13ff6696ddfb3c070fd
Marcus Vynicius <viniciusfla1999@gmail.com>
2026-06-28 16:03:39 +0100

    test: auth tests done

--- ecommerce-api/src/test/java/com/marcusvynicius/ecommerce_api/AuthServiceTest.java
+++ ecommerce-api/src/test/java/com/marcusvynicius/ecommerce_api/AuthServiceTest.java
@@ -1,5 +1,6 @@
 package com.marcusvynicius.ecommerce_api;
 
+import com.marcusvynicius.ecommerce_api.exceptions.InvalidCredentialsException;
 import com.marcusvynicius.ecommerce_api.modules.user.DTOs.AuthRequestDTO;
 import com.marcusvynicius.ecommerce_api.modules.user.Role;
 import com.marcusvynicius.ecommerce_api.modules.user.UserEntity;
@@ -16,9 +17,8 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
@@ -38,7 +38,7 @@ class AuthServiceTest {
     }
 
     @Test
-    void should_be_able_to_sign_in() {
+    public void should_be_able_to_sign_in() {
 
         var login = new AuthRequestDTO("admin@uberweets.local", "Admin@123");
 
@@ -60,6 +60,39 @@ class AuthServiceTest {
 
         verify(userRepository).findByEmail("admin@uberweets.local");
         verify(passwordEncoder).matches("Admin@123", "$we5&345hash");
+    }
+
+    @Test
+    public void should_not_be_able_to_sign_in_with_wrong_email() {
+
+        var request = new AuthRequestDTO("wrongemail@email.com", "Admin@123");
+
+        when(userRepository.findByEmail("wrongemail@email.com")).thenReturn(Optional.empty());
+
+        assertThrows(InvalidCredentialsException.class, () -> authService.execute(request));
+
+        verify(userRepository).findByEmail("wrongemail@email.com");
+        verify(passwordEncoder, never()).matches(any(), any());
+    }
+
+    @Test
+    public void should_not_be_able_to_sign_in_with_wrong_password() {
+
+        var request = new AuthRequestDTO("admin@uberweets.local",  "wrong-password");
+
+        var user =  UserEntity.builder()
+                .email("admin@uberweets.local")
+                .password_hash("$we5&345hash")
+                .role(Role.ADMIN)
+                .build();
+
+        when(userRepository.findByEmail("admin@uberweets.local")).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("wrong-password", "$we5&345hash")).thenReturn(false);
+
+        assertThrows(InvalidCredentialsException.class, () -> authService.execute(request));
+
+        verify(userRepository).findByEmail("admin@uberweets.local");
+        verify(passwordEncoder).matches("wrong-password", "$we5&345hash");
     }
 }
 
